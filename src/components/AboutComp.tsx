94b28b0ce0951a9d4e9dedf7bbded25f102fe4f1
ShaunZap <157569827+ShaunZap@users.noreply.github.com>
2025-06-23 17:20:01 +0530

    Revert "created testing branch and added tests"

--- src/components/AboutComp.tsx
+++ src/components/AboutComp.tsx
@@ -10,27 +10,18 @@ const AboutComp = () => {
         <div className="image-container">
           <img src={AboutData.profilePic} alt="shaun-image" />
         </div>
-        <div className="about-description" data-testid="about-desc">
-          {AboutData.description}
-        </div>
+        <div className="about-description">{AboutData.description}</div>
       </div>
       <div className="about-section-2">
         <div className="hobbies-title">{AboutData.subtitle}</div>
-        <div className="hobbies-description" data-testid="hobbie-desc">
-          {AboutData.description2}
-        </div>
+        <div className="hobbies-description">{AboutData.description2}</div>
         <div className="hobbies-list">
           {Object.entries(AboutData.hobbies).map(([key, category]) => (
             <div className="hobbie-card" key={key}>
               <div className="hobbie-title">{category.title}</div>
               <ul>
                 {category.items.map((item, index) => (
-                  <li
-                    key={index}
-                    data-testid={`hobby-${item}-${index}-${category.title}`}
-                  >
-                    {item}
-                  </li>
+                  <li key={index}>{item}</li>
                 ))}
               </ul>
             </div>
